145aae3b52ba67a26a5e038e6e979d7984373481
songxx <913581148@qq.com>
2017-07-09 17:04:14 +0800

    python爬虫学习，class了解

---
+++ demo1-spider/index.py
@@ -0,0 +1,44 @@
+
+import urllib2, pymysql
+from bs4 import BeautifulSoup
+
+url = "http://www.baidu.com"
+
+response = urllib2.urlopen(url)
+print response.getcode()
+
+html_doc = response.read()
+
+# print html_doc
+
+soup = BeautifulSoup(html_doc, 'html.parser', from_encoding='utf-8')
+
+# links = soup.find_all('img')
+# for link in links:
+#   print link['src']
+
+link_node = soup.find('img', width='270')
+spider_url = link_node['src']
+
+connect = pymysql.Connect(
+    user='root',
+    passwd='123456',
+    db='test',
+    charset='utf8'
+)
+
+cursor = connect.cursor()
+
+# cursor.execute('create table spider (id varchar(20) primary key, url varchar(200))')
+# cursor.execute('insert into spider (id, url) values (%s, %s)', ['1', spider_url])
+# print(cursor.rowcount)
+# connect.commit()
+# cursor.close()
+
+cursor = connect.cursor()
+cursor.execute('select * from spider where id = %s', ('1',))
+values = cursor.fetchall()
+print(values)
+
+cursor.close()
+connect.close()
---
+++ demo1-spider/测试python爬虫.md
@@ -0,0 +1,6 @@
+1：测试python爬虫
+
+- 使用了urllib2网页下载器
+- 使用beautifulsoup4网页解析器
+- 使用mysql保存有用的数据
+- 使用pymysql 与数据库建立连接
